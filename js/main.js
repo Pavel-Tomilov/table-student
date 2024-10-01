319aec221843eb329bfc314b629450479ba83b58
Your Name <you@example.com>
2024-10-01 22:16:44 +0300

    new-commit

--- js/main.js
+++ js/main.js
@@ -47,8 +47,10 @@ const buttonAddStudent = document.getElementById('addStudentClass');
 
 // buttonAddStudent.addEventListener('click', onAddBtn);
 const addForm = document.getElementById('add-form');
+const tbody = document.querySelector('#studentTable tbody');
+tbody.classList.add('stroke')
 
-
+// Очищаем форму после отправки
 
 function clearForm() {
     document.getElementById('firstName').value = '';
@@ -76,96 +78,31 @@ function calculateAge(birthDate) {
 }
 
 
-const copyStudents = [...students]
+function render() {
 
-for (const student of copyStudents) {
-    student.FIO = student.lastName + ' ' + student.firstName + ' ' + student.middleName;
-    student.well = 2024 - student.startYear;
-    student.year = student.startYear + '-' + '2024'
-    student.age = calculateAge(student.birthDate)
-}
-
-
-const tbody = document.querySelector('#studentTable tbody');
-tbody.classList.add('stroke')
-
-copyStudents.forEach(student => {
-
-
-
-    // Создаем ячейки <td>
-    const nameCell = document.createElement('td');
-    nameCell.textContent = student.FIO;
-
-    const facultyCell = document.createElement('td');
-    facultyCell.textContent = student.faculty;
-
-    const birthDateCell = document.createElement('td');
-    birthDateCell.textContent = student.birthDate + ' ' + '(' + student.age + ' ' + 'лет' + ')';
-
-    const startYearCell = document.createElement('td');
-    startYearCell.textContent = student.year + ' ' + '(' + student.well + ' ' + 'курс' + ')';
-
-    const row = document.createElement('tr');
-
-    // Добавляем ячейки в строку
-    row.appendChild(nameCell);
-    row.appendChild(facultyCell);
-    row.appendChild(birthDateCell);
-    row.appendChild(startYearCell);
-
-    // Добавляем строку в <tbody>
-    tbody.appendChild(row);
-});
-
-
-
-
-addForm.addEventListener('submit', function (event) {
-    event.preventDefault()
-
-    students.push({
-        firstName: document.getElementById('firstName').value,
-        lastName: document.getElementById('lastName').value,
-        middleName: document.getElementById('middleName').value,
-        birthDate: document.getElementById('birthDate').value,
-        startYear: document.getElementById('startYear').value,
-        faculty: document.getElementById('faculty').value,
-
-
-    })
-console.log(students);
-    tbody.innerHTML = ''
+    tbody.innerHTML = '';
 
     const copyStudents = [...students]
 
     for (const student of copyStudents) {
         student.FIO = student.lastName + ' ' + student.firstName + ' ' + student.middleName;
         student.well = 2024 - student.startYear;
-        student.year = student.startYear + '-' + '2024';
-        student.age = calculateAge(student.birthDate);
- 
+        student.year = student.startYear + '-' + '2024'
+        student.age = calculateAge(student.birthDate)
     }
 
 
 
     copyStudents.forEach(student => {
-
-
-
         // Создаем ячейки <td>
         const nameCell = document.createElement('td');
         nameCell.textContent = student.FIO;
-
         const facultyCell = document.createElement('td');
         facultyCell.textContent = student.faculty;
-
         const birthDateCell = document.createElement('td');
         birthDateCell.textContent = student.birthDate + ' ' + '(' + student.age + ' ' + 'лет' + ')';
-
         const startYearCell = document.createElement('td');
         startYearCell.textContent = student.year + ' ' + '(' + student.well + ' ' + 'курс' + ')';
-
         const row = document.createElement('tr');
 
         // Добавляем ячейки в строку
@@ -178,10 +115,28 @@ console.log(students);
         tbody.appendChild(row);
     });
 
+}
+
+render()
+
+// Добавляем студента из формы в массив
+
+addForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    students.push({
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        middleName: document.getElementById('middleName').value,
+        birthDate: document.getElementById('birthDate').value,
+        startYear: document.getElementById('startYear').value,
+        faculty: document.getElementById('faculty').value,
+    })
+
+
+    render()
     clearForm();
-
 })
-
 
 
 // const validateStudents = (a,b,c,d...)
